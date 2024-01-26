62a05557a1c06dd4a41d9096944d8ccd12c75fca
janhavikhopade <157823561+janhavikhopade@users.noreply.github.com>
2024-01-26 22:53:53 +0530

    Add files via upload

---
+++ NumberGuessingGame.java
@@ -0,0 +1,36 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+
+	    public static void main(String[] args) {
+		   Random random = new Random();
+		       int secretNumber = random.nextInt(100) + 1;
+                       int guess;
+		       int attempts = 0;
+
+			       Scanner scanner = new Scanner(System.in);
+
+					  System.out.println("Welcome to the Number Guessing Game!");
+					 System.out.println("Try to guess the number between 1 and 100.");
+
+						  do {
+								  System.out.print("Enter your guess: ");
+							          guess = scanner.nextInt();
+							          attempts++;
+
+						   if (guess == secretNumber) {
+							         System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+	                                          																			                } else if (guess < secretNumber) {
+			                                      																				                 System.out.println("Too low! Try again.");
+						                 																		        } else {
+				        																						                    System.out.println("Too high! Try again.");
+																													                }
+
+									            } while (guess != secretNumber);
+
+										            scanner.close();
+										        }
+}
+
